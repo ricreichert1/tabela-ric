508c067df393f7e3c07fd9f09b381e128b8b0665
ricreichert1 <68476137+ricreichert1@users.noreply.github.com>
2024-11-11 16:17:08 -0300

    tabela+get

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+async function atualizarTabela() {
+    const url = 'http://localhost:3000/rick/list'
 
+    try {
+        const resposta = await fetch(url)
+        const registros = await resposta.json()
 
-let y = {nomeInput: 'jhyu', idadeInput: '546', alturaInput: '654', pesoInput: '4'}
+        let tabelaHTML = ''
+        registros.forEach(function(registro) {
+            tabelaHTML += `
+                <tr>
+                    <td>${registro.nome}</td>
+                    <td>${registro.idade}</td>
+                    <td>${registro.altura}</td>
+                    <td>${registro.peso}</td>
+                </tr>
+            `
+        })
 
-function objToJsonTest(test) {
-    console.log(test)
-    let x = JSON.stringify(test)
-    console.log(x)
-    enviarDados(x)
+        document.getElementById('tabelaRegistros').innerHTML = tabelaHTML
+    } catch (error) {
+        console.error('Erro ao atualizar a tabela:', error)
+    }
 }
 
-async function enviarDados(x) {
-    const url = 'http://localhost:3000/rick/add';
-    console.log("Enviando dados para:", url);
+
+async function enviarDados(dados) {
+    const url = 'http://localhost:3000/rick/add'
+    console.log("Enviando dados para:", url)
     
     try {
         const resposta = await fetch(url, {
@@ -19,27 +34,40 @@ async function enviarDados(x) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: x,
-        });
-
-        const resultado = await resposta.json();
-        console.log(resultado);
+            body: dados,  
+        })
+        const resultado = await resposta.json()
+        console.log(resultado)
+        atualizarTabela()
     } catch (error) {
-        console.error('Erro ao enviar dados:', error.message);
+        console.error('Erro ao enviar dados:', error.message)
     }
 }
 
+function toJson(teste) {
+    console.log(teste)
+    let dota = JSON.stringify(teste)
+    console.log(dota)
+    enviarDados(dota)
+}
 
-$(document).ready(function(){
-   $('#saveButton').on('click', function(){
-       var obj = {}
-       $('input').each(function(index, element){
-           var val = $(this).val()
-           var id = $(this).attr('id') 
-           obj[id] = val
-       })
-       console.log(obj)
-    //    enviarDados(obj)
-    objToJsonTest(obj)
-   })
+$(document).ready(function() {
+    $('#saveButton').on('click', function() {
+        var obj = {}
+        $('input').each(function(index, element) {
+            var val = $(this).val()
+            var id = $(this).attr('id')
+            obj[id] = val
+        })
+
+        toJson(obj)
+
+
+        $('#nomeInput').val('')
+        $('#idadeInput').val('')
+        $('#alturaInput').val('')
+        $('#pesoInput').val('')
+    })
+
+    atualizarTabela()
 })
